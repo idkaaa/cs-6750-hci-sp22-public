daeb15e173ce9ca9044cebc0a72a01df0c6bb3e9
idkaaa <chris.hiles@gmail.com>
2022-04-30 14:08:07 -0400

    moved readings

--- readings/combine-html.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#find . -name "*.ht*" | while read i; do pandoc -s "$i" ; done
-FILES=$(find . -name "*.ht*")
-DIR=pwd
-pandoc -s $FILES -o "$DIR/output.html"
--- readings/remove-filename-spaces.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-cat ./files_with_spaces.txt | while read i; do $i ; done
